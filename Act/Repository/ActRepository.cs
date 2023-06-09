7f9cea310c62722731a6a99418648486d8d934d9
elizzrss <korolkova.elizaveta4@gmail.com>
2023-06-09 18:23:04 +0500

    Добавлен репозиторий для актов

---
+++ Act/Repository/ActRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IS_5
+{
+    public class ActRepository
+    {
+        public List<Act> GetActs()
+        {
+            return TestData.Acts;
+        }
+
+        public void DeleteActFromRepository(int id)
+        {
+            TestData.Acts.Remove(GetAct(id));
+        }
+
+        public Act GetAct(int id)
+        {
+            return TestData.Acts.Where(a => a.Id == id).FirstOrDefault();
+        }
+
+        public void AddActToRepository(Act a) => TestData.Acts.Add(a);
+    }
+}
